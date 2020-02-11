49f7427944f9cfeac575e525c4a8986ee84e41b9
Jack Taylor <taylorjh@whitman.edu>
2020-02-11 09:03:28 -0800

    Updated project

--- src/scaleplayer/ScalePlayer.java
+++ src/scaleplayer/ScalePlayer.java
@@ -8,8 +8,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
+import javafx.geometry.Insets;
+import java.util.*;
+import static javafx.application.Application.launch;
+
 
 /**
  * This JavaFX app lets the user play scales.
@@ -19,23 +26,79 @@ import javafx.stage.Stage;
  * @since January 26, 2017
  */
 public class ScalePlayer extends Application {
+   
+    MidiPlayer player;
+   
+   private void populate_notes(int start){
+       int note_incriments[]  = new int[]{2,2,1,2,2,2,1};
+       int note_position = 1;
+       for(int i = 0; i < 7; i++){
+           player.addNote(start, 60, note_position, 2, 1, 0);
+           start += note_incriments[i];
+           note_position += 2;
+       }
+        for(int j = 6; j > -1; j--){
+           player.addNote(start, 60, note_position, 2, 1, 0);
+           start -= note_incriments[j];
+           note_position += 2;
+       }
+   }
+   EventHandler<ActionEvent> play_event = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) 
+            { 
+                TextInputDialog td = new TextInputDialog(""); 
+                td.setHeaderText("Choose a note");
+                td.showAndWait(); 
+                int note_value = Integer.parseInt(td.getEditor().getText());
+                System.out.println(note_value);
+                
+                populate_notes(note_value);
+                System.out.println("Playing");
+                player.play();
+            } 
+        }; 
+   
+   EventHandler<ActionEvent> stop_event = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) 
+            { 
+                player.stop();
+                player.clear();
+            } 
+        }; 
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        player = new MidiPlayer(4, 60);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Button main_buttons[] = new Button[3];
+        main_buttons[0] = new Button("Play Scale");
+        main_buttons[1] = new Button("Stop playing");
+        main_buttons[2] = new Button("Exit");
         
-        Scene scene = new Scene(root, 300, 250);
+      
+        
+        
+        HBox buttons_top = new HBox();
+        HBox buttons_bottom = new HBox();
+        buttons_top.getChildren().add(main_buttons[0]);
+        buttons_top.getChildren().add(main_buttons[1]);
+        buttons_bottom.getChildren().add(main_buttons[2]);
+        
+        BorderPane main_screen = new BorderPane();
+        main_screen.setCenter(buttons_top);
+        main_screen.setBottom(buttons_bottom);
+        main_screen.setLayoutX(100);
+        main_screen.setLayoutY(30);
+    
+        Scene scene = new Scene(main_screen, 400, 100);
+        
+        
+        
+
+        
+        
+        main_buttons[0].setOnAction(play_event);
+        main_buttons[1].setOnAction(stop_event);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
